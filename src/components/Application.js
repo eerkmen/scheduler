e742b03f4cd8503bfbf7f1be6f66718e0f23c866
eerkmen <efeerkmen@gmail.com>
2023-04-16 13:00:00 +0000

    add logo img to sidebar

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,13 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        
+{/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
