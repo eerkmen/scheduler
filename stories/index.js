b3ad4d66b289d95f9747e93d5d1664cafaf266c2
eerkmen <efeerkmen@gmail.com>
2023-04-19 15:00:00 +0000

    add comments to storiesOf module for daylistitem in stories/index.js

--- stories/index.js
+++ stories/index.js
@@ -32,4 +32,5 @@ storiesOf("Button", module)
   .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
   .add("Full", () => <DayListItem name="Monday" spots={0} />)
   .add("Clickable", () => (
-    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5}
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
+  ));
