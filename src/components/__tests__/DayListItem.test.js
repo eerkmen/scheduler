8a1e1b8d0735b1b234a04baf0537ba511d8892fe
eerkmen <efeerkmen@gmail.com>
2023-04-22 10:00:00 +0000

    add renders "2 spots remaining" when there are 2 spots test to daylistitem.test.js file

--- src/components/__tests__/DayListItem.test.js
+++ src/components/__tests__/DayListItem.test.js
@@ -19,3 +19,8 @@ it("renders '1 spot remaining' when there is 1 spot", () => {
   const { getByText } = render(<DayListItem name="Monday" spots={1} />);
   expect(getByText("1 spot remaining")).toBeInTheDocument();
 });
+
+it("renders '2 spots remaining' when there are 2 spots", () => {
+  const { getByText } = render(<DayListItem name="Monday" spots={2} />);
+  expect(getByText("2 spots remaining")).toBeInTheDocument();
+});
