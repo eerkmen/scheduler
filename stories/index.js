79ddede89c7084dae34fa2e572ed27ecf8c6b499
eerkmen <efeerkmen@gmail.com>
2023-04-23 10:00:00 +0000

    add stories of DayList to stories/index.js file

--- stories/index.js
+++ stories/index.js
@@ -54,4 +54,16 @@ storiesOf("Button", module)
     },
   ];
   
-  
+  storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ))
+  .add("Wednesday", () => (
+      <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+  ));
