77b8093eaa5136a67a106180a22151ccb1a00c61
eerkmen <efeerkmen@gmail.com>
2023-04-26 10:00:00 +0000

    add daylist object to daylist.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,6 +3,14 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   const { days, value, onChange } = props;
- 
-  return <ul></ul>;
+  const dayList = das.map(d => (
+    <DayListIte
+      key={d.id
+      name={d.name
+      spots={d.spots
+      selected={d.nae === value}
+      setDay={onChane}
+    /
+  ))
+  return <ul>{dayList}</ul>;
 }
