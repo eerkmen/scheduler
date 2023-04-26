2088a3579f17b4d0f219800f88598499918ce306
eerkmen <efeerkmen@gmail.com>
2023-04-26 10:00:00 +0000

    bug-fix:daylist object to daylist.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,14 +3,14 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   const { days, value, onChange } = props;
-  const dayList = das.map(d => (
-    <DayListIte
-      key={d.id
-      name={d.name
-      spots={d.spots
-      selected={d.nae === value}
-      setDay={onChane}
-    /
-  ))
+  const dayList = days.map(d => (
+    <DayListItem
+      key={d.id}
+      name={d.name}
+      spots={d.spots}
+      selected={d.name === value}
+      setDay={onChange}
+    />
+  ));
   return <ul>{dayList}</ul>;
 }
