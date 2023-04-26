f44541a7b288119ad70f1d6215536922888581b2
eerkmen <efeerkmen@gmail.com>
2023-04-26 10:00:00 +0000

    add props const for readability

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,6 +2,7 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-
+  const { days, value, onChange } = props;
+ 
   return <ul></ul>;
 }
