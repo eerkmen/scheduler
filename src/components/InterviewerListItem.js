997d4212ade7280c8fa706772015d86c60422a78
eerkmen <efeerkmen@gmail.com>
2023-04-27 10:00:00 +0000

    add const interviewerclass to function in interviewerlistitem.js

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,7 +4,9 @@ import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
   const { name, avatar, selected, setInterviewer } = props;
- 
+  const InterviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected,
+  });
 
   return ()
 }
