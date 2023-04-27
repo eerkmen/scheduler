f8740f9af4f230c6d596913faa6e1c472a50540a
eerkmen <efeerkmen@gmail.com>
2023-04-27 10:00:00 +0000

    add li to return statement in function at interviewerlistitem.js

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,5 +8,10 @@ export default function InterviewerListItem(props) {
     "interviewers__item--selected": selected,
   });
 
-  return ()
+  return (
+    <li className={InterviewerClass} onClick={setInterviewer}>
+      <img className="interviewers__item-image" src={avatar} alt={name} />
+      {selected && name}
+    </li>
+  );
 }
