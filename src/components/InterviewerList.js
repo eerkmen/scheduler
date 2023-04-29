d3e3a17124bf2e42ec770ab517bc4cf0ef2234e0
eerkmen <efeerkmen@gmail.com>
2023-04-29 10:00:00 +0000

    add const interviewer items to function in interviewerlist.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,7 +5,17 @@ import "components/InterviewerList.scss";
 
 const InterviewerList = props => {
   const { interviewers, onChange, value } = props;
-  
+  const interviewerItems = interviewers.map(n => {
+    return (
+      <InterviewerListItem
+        key={n.id}
+        name={n.name}
+        avatar={n.avatar}
+        setInterviewer={() => onChange(n.id)}
+        selected={n.id === value}
+      />
+    );
+  });
 
   return (
     <section className="interviewers">
@@ -16,7 +26,6 @@ const InterviewerList = props => {
     </section>
   );
 };
-
 };
 
 export default InterviewerList;
