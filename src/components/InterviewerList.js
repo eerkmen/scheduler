f791faf48205487178d796894ad7beec40d474cf
eerkmen <efeerkmen@gmail.com>
2023-04-29 10:00:00 +0000

    add return statement to function in interviewerlist.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,3 +2,19 @@ import React from "react";
 import propTypes from "prop-types";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+
+const InterviewerList = props => {
+ 
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewerItems}
+        </ul>
+    </section>
+  );
+};
+
+};
+
+export default InterviewerList;
