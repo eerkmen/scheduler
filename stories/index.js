a5de21b6bccb8b7c4238f41b100a2683fcf68289
eerkmen <efeerkmen@gmail.com>
2023-04-30 10:00:00 +0000

    add daylistitem storiesof to stories/index.js

--- stories/index.js
+++ stories/index.js
@@ -58,6 +58,17 @@ storiesOf("Button", module)
     </Button>
   ));
 
+storiesOf("DayListItem", module) 
+.addParameters({
+  backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+}) 
+.add("Unselected", () => <DayListItem name="Monday" spots={5} />) 
+.add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+.add("Full", () => <DayListItem name="Monday" spots={0} />)
+.add("Clickable", () => (
+  <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
+));
+
 storiesOf("DayList", module)
 .addParameters({
   backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
