70f95f9db378bdf909c2333b85038a5d550549be
eerkmen <efeerkmen@gmail.com>
2023-05-03 10:00:00 +0000

    add storyof appointment module to stories/index.js

--- stories/index.js
+++ stories/index.js
@@ -134,3 +134,11 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+  storiesOf("Appointment", module)
+    .addParameters({
+     backgrounds: [{ name: "white", value: "#fff", default: true }]
+   })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time="12pm" />);
+
