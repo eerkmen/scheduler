c29faf865f85adb3469becca8279f10bcf5db3be
eerkmen <efeerkmen@gmail.com>
2023-05-06 10:00:00 +0000

    add storyof show to stories/index.js

--- stories/index.js
+++ stories/index.js
@@ -143,4 +143,12 @@ storiesOf("InterviewerList", module)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add("Header", () => <Header time="12pm"/>)
     .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+    .add("Show", () => (
+      <Show
+        student={"Lydia Miller-Jones"}
+        interviewer={interviewer}
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
+      />
+    ))
     
