f38fdd72a488dc8a5a9dfe1437b1185f0bb491c2
eerkmen <efeerkmen@gmail.com>
2023-05-07 10:00:00 +0000

    add const usestate to export function in form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,5 +3,8 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
  
 };
