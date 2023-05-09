9b5e4519b7b299df7d6c8ef8e0af97333ae3c1eb
eerkmen <efeerkmen@gmail.com>
2023-05-09 10:00:00 +0000

    import header,show,empty components to Appointment/index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import "./styles.scss";
+import Header from './Header';
+import Empty from "./Empty";
+import Show from "./Show";
+
+
+
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
-
+    <article className="appointment" data-testid="appointment">
+      {/* <Header time={props.time} /> */}
+    </article>
   )
 }
