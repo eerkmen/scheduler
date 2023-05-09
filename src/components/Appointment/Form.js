350470b5f23e2f80db9ff1f2aeeffdf7e09e22c0
eerkmen <efeerkmen@gmail.com>
2023-05-09 10:00:00 +0000

    add return statement to export function in form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -31,5 +31,33 @@ export default function Form(props) {
     setError("");
     onSave(student, interviewer);
   };
- 
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder={"Enter Student Name"}
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
+          />
+        </form>
+        <div className="appointment__validation">{error}</div>
+        <InterviewerList
+          interviewers={interviewers}
+          onChange={setInterviewer}
+          value={interviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => validating()}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
 };
