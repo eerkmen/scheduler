6ac39033c09b1b13f38806470d576fcb7c82c068
eerkmen <efeerkmen@gmail.com>
2023-05-13 10:00:00 +0000

    add function back to export function in useVisualMode.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,5 +4,14 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
  
+  function back(){
+    if (history[history.length -1] !== initial) {
+      history.pop();
+      setHistory(history);
+      setMode(history[history.length - 1]);
+    }
+  }  
+
+  return { transition, mode, back };
 }
   // removes the last item in the history array, and sets the mode to be the second to last s
