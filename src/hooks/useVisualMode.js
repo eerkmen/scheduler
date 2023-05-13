66b9971a67fc12f191e1dba547f4ea78e5fa0e50
eerkmen <efeerkmen@gmail.com>
2023-05-13 10:00:00 +0000

    add useState to export function in useVisualMode.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,8 @@
 import {useState} from 'react';
 
 export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
  
 }
   // removes the last item in the history array, and sets the mode to be the second to last s
