a763db327fc20f107fd35ededc3a5d6f5428eb49
eerkmen <efeerkmen@gmail.com>
2023-05-14 10:00:00 +0000

    add const for appointments in appointment/index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import "./styles.scss";
+import useVisualMode from "hooks/useVisualMode";
 import Header from './Header';
 import Empty from "./Empty";
 import Show from "./Show";
-
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
 import Error from './Error';
 
+const EMPTY = "EMPTY";
+const CREATE = "CREATE";
+const SAVING = "SAVING";
+const SHOW = "SHOW";
+const EDIT = "EDIT";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+const ERROR_DELETE = "ERROR_DELETE";
+const ERROR_SAVE = "ERROR-SAVE";
 
 export default function Appointment(props) {
   return (
