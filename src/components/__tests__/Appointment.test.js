bca3d70f039ac3f228056e32b001f4ff78c6186f
eerkmen <efeerkmen@gmail.com>
2023-05-18 10:00:00 +0000

    add comments to appointment.test.js

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,19 @@
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
+
+
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
+
+
+/*
+  We import the component that we are testing
+*/
+
+
+/*
+  A test that renders a React Component
+*/
