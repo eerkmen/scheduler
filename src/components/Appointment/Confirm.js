7426fb33cf969e13cdd7f4e6745f5082939ed02c
eerkmen <efeerkmen@gmail.com>
2023-05-23 11:00:00 +0000

    add comments in confirm.js

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -2,18 +2,23 @@ import React from "react";
 import Button from "components/Button";
 
 export default function Confirm(props) {
+  // Destructure props for better readability
   const { message, onConfirm, onCancel } = props;
+
   return (
     <main className="appointment__card appointment__card--confirm">
+      {/* Display the confirmation message */}
       <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
+        {/* Cancel button */}
         <Button danger onClick={onCancel}>
           Cancel
         </Button>
-        <Button danger onClick={onConfirm()}>
-         Confirm
-       </Button>
+        {/* Confirm button */}
+        <Button danger onClick={onConfirm}>
+          Confirm
+        </Button>
       </section>
     </main>
   );
-}; 
+};
