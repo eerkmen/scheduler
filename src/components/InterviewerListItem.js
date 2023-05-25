8d1398b6f2816c557a75f26b2509e68117aed065
eerkmen <efeerkmen@gmail.com>
2023-05-25 11:00:00 +0000

    add comments to interviewerlistitem.js

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,13 +3,17 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
+  // Destructure the props for easier access
   const { name, avatar, selected, setInterviewer } = props;
-  const InterviewerClass = classNames("interviewers__item", {
+
+  // Use classNames library to conditionally apply CSS classes
+  const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": selected,
   });
 
+  // Render the InterviewerListItem component
   return (
-    <li className={InterviewerClass} onClick={setInterviewer}>
+    <li className={interviewerClass} onClick={setInterviewer}>
       <img className="interviewers__item-image" src={avatar} alt={name} />
       {selected && name}
     </li>
