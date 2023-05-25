077858c51f0eb9aaccd04dd4a07aba5dcaffd943
eerkmen <efeerkmen@gmail.com>
2023-05-25 11:00:00 +0000

    bug-fix:remove react import,update functions useVisualMode.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,23 +1,31 @@
-import {useState} from 'react';
-
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
  
-  function back(){
-    if (history[history.length -1] !== initial) {
-      history.pop();
-      setHistory(history);
-      setMode(history[history.length - 1]);
+  function back() {
+    if (history[history.length - 1] !== initial) {
+      setHistory(prev => {
+        const newHistory = [...prev];
+        newHistory.pop();
+        return newHistory;
+      });
+      setMode(history[history.length - 2]);
     }
   }  
+
   function transition(mode, replace = false) {
     if (replace) {
       setMode(mode);
-      history.pop();
-    } 
-    setMode(mode);
-    setHistory(prev => [...prev, mode]);
+      setHistory(prev => {
+        const newHistory = [...prev];
+        newHistory.pop();
+        return [...newHistory, mode];
+      });
+    } else {
+      setMode(mode);
+      setHistory(prev => [...prev, mode]);
+    }
   }
+
   return { mode, transition, back };
 }
