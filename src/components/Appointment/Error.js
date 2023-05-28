a9b6ab5ee9f9221e919a1121e9431d22d3579619
eerkmen <efeerkmen@gmail.com>
2023-05-28 00:00:00 +0000

    add comment to error.js

--- src/components/Appointment/Error.js
+++ src/components/Appointment/Error.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Error(props) {
+  // Destructure props for better readability
   const { message, onClose } = props;
   return (
     <main className="appointment__card appointment__card--error">
