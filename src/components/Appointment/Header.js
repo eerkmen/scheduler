13714eca5b56c917521531dade42b3ade823f5ca
eerkmen <efeerkmen@gmail.com>
2023-05-31 00:00:00 +0000

    readability:add comments to Header.js

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
+
+/**
+ * Component for displaying the header of an appointment.
+ * @param {Object} props - The props that are passed to the component.
+ * @param {string} props.time - The time value to be displayed in the header.
+ * @returns {JSX.Element} - Rendered component.
+ */
 export default function Header(props) {
   // Destructure props for better readability
   const { time } = props;
+
   return (
     <header className="appointment__time">
       <h4 className="text--semi-bold">{time}</h4>
