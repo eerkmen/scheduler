0a066e3a74392e1bc5868f89b6c320ca88a50ada
eerkmen <efeerkmen@gmail.com>
2023-05-31 00:00:00 +0000

    readability:add comments to Empty.js

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,15 +1,23 @@
 import React from "react";
+
+/**
+ * Component for displaying an empty spot with an add button.
+ * @param {Object} props - The props that are passed to the component.
+ * @param {function} props.onAdd - The function to be called when the add button is clicked.
+ * @returns {JSX.Element} - Rendered component.
+ */
 export default function Empty(props) {
-    // Destructure props for better readability
-    const { onAdd } = props;
+  // Destructure props for better readability
+  const { onAdd } = props;
+
   return (
     <main className="appointment__add">
-     <img
-      className="appointment__add-button"
-      src="images/add.png"
-      alt="Add"
-      onClick={onAdd}
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={onAdd}
       />
     </main>
   );
-}
+};
