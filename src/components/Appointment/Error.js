80ae60c6bdfbb5459294fa0ed5ac8fc9d7d6a4be
eerkmen <efeerkmen@gmail.com>
2023-05-31 00:00:00 +0000

    readability:add comments to Error.js

--- src/components/Appointment/Error.js
+++ src/components/Appointment/Error.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+/**
+ * Component for displaying an error message.
+ * @param {Object} props - The props that are passed to the component.
+ * @param {string} props.message - The error message to be displayed.
+ * @param {function} props.onClose - The function to be called when the error message is closed.
+ * @returns {JSX.Element} - Rendered component.
+ */
 export default function Error(props) {
   // Destructure props for better readability
   const { message, onClose } = props;
+
   return (
     <main className="appointment__card appointment__card--error">
       <section className="appointment__error-message">
